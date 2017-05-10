ef7e0872e2ff1dc7ec472f59c15455b56b81957c
Luiz Gustavo Bragança Dos Santos <luiz.braganca@sga.pucminas.br>
2017-05-10 20:01:37 -0300

    tela do banco de dados

--- app/src/main/java/br/com/gustavo/luiz/trabalhopratico2_lddm/BancoDeDados.java
+++ app/src/main/java/br/com/gustavo/luiz/trabalhopratico2_lddm/BancoDeDados.java
@@ -3,10 +3,11 @@ package br.com.gustavo.luiz.trabalhopratico2_lddm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BancoDeDados extends AppCompatActivity {
-
+public class BancoDeDados extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banco_de_dados);
     }
