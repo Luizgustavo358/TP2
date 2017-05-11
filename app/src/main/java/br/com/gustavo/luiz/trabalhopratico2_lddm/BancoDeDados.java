ff4bd1effc26c91d5aa4d64ef709cf066787418c
Luiz Gustavo Bragança Dos Santos <luiz.braganca@sga.pucminas.br>
2017-05-10 21:28:49 -0300

    hue

--- app/src/main/java/br/com/gustavo/luiz/trabalhopratico2_lddm/BancoDeDados.java
+++ app/src/main/java/br/com/gustavo/luiz/trabalhopratico2_lddm/BancoDeDados.java
@@ -2,13 +2,37 @@ package br.com.gustavo.luiz.trabalhopratico2_lddm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BancoDeDados extends AppCompatActivity
 {
+    public ArrayList<Coordenadas> imageArray = new ArrayList<>();
+    public ListView dataList;
+    CoordImageAdapter imageAdapter;
+    public DbHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banco_de_dados);
+
+        dataList = (ListView) findViewById(R.id.listView);
+
+        db = new DbHelper(this);
+
+        List<Coordenadas> coordenadas = db.getAllCoordenadas();
+
+        for(Coordenadas c : coordenadas)
+        {
+            imageArray.add(c);
+        }// end for
+
+        imageAdapter = new CoordImageAdapter(this, R.layout.content_db, imageArray);
+
+        dataList.setAdapter(imageAdapter);
     }
 }
